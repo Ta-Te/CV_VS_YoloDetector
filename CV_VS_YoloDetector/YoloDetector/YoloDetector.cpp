4bbd2dcc5f6f7e08494f893ef1acc3ac8b7d7b62
Ta-Te <m456@ntu.edu.tw>
2017-08-16 00:00:25 +0800

    Add files via upload

---
+++ CV_VS_YoloDetector/YoloDetector/YoloDetector.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <iomanip> 
+#include <string>
+#include <vector>
+#include <fstream>
+#include <thread>
+#include <atomic>
+
+#define OPENCV
+
+#include "yolo_v2_class.hpp"	// imported functions from DLL
+
+#ifdef OPENCV
+#include <opencv2/opencv.hpp>			// C++
+#include "opencv2/core/version.hpp"
+#ifndef CV_VERSION_EPOCH
+#include "opencv2/videoio/videoio.hpp"
+#pragma comment(lib, "opencv_world320.lib")  
+#else
+#pragma comment(lib, "opencv_core2413.lib")  
+#pragma comment(lib, "opencv_imgproc2413.lib")  
+#pragma comment(lib, "opencv_highgui2413.lib") 
+#endif
+
+
+void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, unsigned int wait_msec = 0) {
+	for (auto &i : result_vec) {
+		cv::Scalar color(60, 160, 260);
+		cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 5);
+		if (obj_names.size() > i.obj_id) {
+			std::string obj_name = obj_names[i.obj_id];
+			if (i.track_id > 0) obj_name += " - " + std::to_string(i.track_id);
+			cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
+			int const max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+			cv::rectangle(mat_img, cv::Point2f(std::max((int)i.x - 3, 0), std::max((int)i.y - 30, 0)), 
+				cv::Point2f(std::min((int)i.x + max_width, mat_img.cols-1), std::min((int)i.y, mat_img.rows-1)), 
+				color, CV_FILLED, 8, 0);
+			putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 10), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 0, 0), 2);
+		}
+	}
+	cv::imshow("window name", mat_img);
+	cv::waitKey(wait_msec);
+}
+#endif	// OPENCV
+
+
+void show_result(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names) {
+	for (auto &i : result_vec) {
+		if (obj_names.size() > i.obj_id) std::cout << obj_names[i.obj_id] << " - ";
+		std::cout << "obj_id = " << i.obj_id << ",  x = " << i.x << ", y = " << i.y 
+			<< ", w = " << i.w << ", h = " << i.h
+			<< std::setprecision(3) << ", prob = " << i.prob << std::endl;
+	}
+}
+
+std::vector<std::string> objects_names_from_file(std::string const filename) {
+	std::ifstream file(filename);
+	std::vector<std::string> file_lines;
+	if (!file.is_open()) return file_lines;
+	for(std::string line; file >> line;) file_lines.push_back(line);
+	std::cout << "object names loaded \n";
+	return file_lines;
+}
+
+
+int main(int argc, char *argv[])
+{
+	std::string filename;
+	if (argc > 1) filename = argv[1];
+
+
+	Detector detector("yolo.cfg", "yolo.weights");
+	auto obj_names = objects_names_from_file("data/yolo.names");
+
+//	Detector detector("yolo-voc.cfg", "yolo-voc.weights");
+//	auto obj_names = objects_names_from_file("data/voc.names");
+
+//	Detector detector("yolo9000.cfg", "yolo9000.weights");
+//	auto obj_names = objects_names_from_file("data/9k.names");
+
+	while (true) 
+	{		
+		std::cout << "input image or video filename: ";
+		if(filename.size() == 0) std::cin >> filename;
+		if (filename.size() == 0) break;
+		
+		try {
+#ifdef OPENCV
+			std::string const file_ext = filename.substr(filename.find_last_of(".") + 1);
+			std::string const protocol = filename.substr(0, 4);
+			if (file_ext == "avi" || file_ext == "mp4" || file_ext == "mjpg" || file_ext == "mov" || 	// video file
+				protocol == "rtsp" || protocol == "http")	// video network stream
+			{
+				cv::Mat frame, prev_frame, det_frame;
+				std::vector<bbox_t> result_vec, thread_result_vec;
+				detector.nms = 0.02;	// comment it - if track_id is not required
+				std::thread td([]() {});
+				std::atomic<int> ready_flag;
+				ready_flag = false;
+				cv::VideoCapture cap(filename);
+				for (; cap >> frame, cap.isOpened();) {
+					td.join();
+					result_vec = thread_result_vec;
+					det_frame = frame;
+					td = std::thread([&]() { thread_result_vec = detector.detect(det_frame, 0.24, true); ready_flag = true; });
+
+					if (!prev_frame.empty()) {
+						result_vec = detector.tracking(result_vec);	// comment it - if track_id is not required
+						draw_boxes(prev_frame, result_vec, obj_names, 3);
+						show_result(result_vec, obj_names);
+					}
+					prev_frame = frame;
+					//if (protocol == "rtsp" || protocol == "http") do { cap.grab(); } while (!ready_flag);	// use if cam-fps 2x or more than dnn-fps
+					ready_flag = false;
+				}
+			}
+			else if (file_ext == "txt") {	// list of image files
+				std::ifstream file(filename);
+				if (!file.is_open()) std::cout << "File not found! \n";
+				else 
+					for (std::string line; file >> line;) {
+						std::cout << line << std::endl;
+						cv::Mat mat_img = cv::imread(line);
+						std::vector<bbox_t> result_vec = detector.detect(mat_img);
+						show_result(result_vec, obj_names);
+						//draw_boxes(mat_img, result_vec, obj_names);
+						//cv::imwrite("res_" + line, mat_img);
+					}
+				
+			}
+			else {	// image file
+				cv::Mat mat_img = cv::imread(filename);
+				std::vector<bbox_t> result_vec = detector.detect(mat_img);
+				result_vec = detector.tracking(result_vec);	// comment it - if track_id is not required
+				draw_boxes(mat_img, result_vec, obj_names);
+				show_result(result_vec, obj_names);
+			}
+#else
+			//std::vector<bbox_t> result_vec = detector.detect(filename);
+
+			auto img = detector.load_image(filename);
+			std::vector<bbox_t> result_vec = detector.detect(img);
+			detector.free_image(img);
+			show_result(result_vec, obj_names);
+#endif			
+		}
+		catch (std::exception &e) { std::cerr << "exception: " << e.what() << "\n"; getchar(); }
+		catch (...) { std::cerr << "unknown exception \n"; getchar(); }
+		filename.clear();
+	}
+
+	return 0;
+}
---
+++ CV_VS_YoloDetector/x64/Release/YoloDetector_coco.cmd
@@ -0,0 +1,6 @@
+
+
+YoloDetector.exe dog.jpg data/coco.data yolo.cfg yolo.weights -i 0 -thresh 0.3
+
+
+pause
